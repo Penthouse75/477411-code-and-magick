59319cc80becaceed11408da18cd32ba3a8adb21
PenthouseEagle <oorlov@gmail.com>
2017-08-25 15:55:44 +0300

    event handler

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,57 @@
 'use strict';
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+/*
+ * Open/Close Setup dialog
+ */
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = document.querySelector('.setup-close');
 
-var similarListElement = userDialog.querySelector('.setup-similar-list');
+setupOpen.addEventListener('click', function() {
+  setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', function() {
+    if (evt.keyCode === 27) {
+      setup.classList.add('hidden');
+    }
+  });
+});
+
+setupClose.addEventListener('click', function() {
+  setup.classList.add('hidden');
+});
+
+/*
+ * Tuning Setup dialog
+ */
+var userNameInput = setup.querySelector('.setup-user-name');
+
+userNameInput.addEventListener('invalid', function (evt) {
+  if (!userNameInput.validity.valid) {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    }
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length < 2) {
+    target.setCustomValidity('');
+  } else {
+    target.setCustomValidity('');
+  }
+});
+/*
+ * Create wizard
+ */
+var similarListElement = setup.querySelector('.setup-similar-list');
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
@@ -66,4 +114,7 @@ for (var i = 0; i < wizards.length; i++) {
 }
 similarListElement.appendChild(fragment);
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+/*
+ * Show wizard
+ */
+setup.querySelector('.setup-similar').classList.remove('hidden');
